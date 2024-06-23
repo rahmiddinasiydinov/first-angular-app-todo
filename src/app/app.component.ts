4599fd394c0520cf99346b121998d8bf880adb96
Rahmiddin Asiydinov <81851084+rahmiddinasiydinov@users.noreply.github.com>
2024-06-23 16:52:44 +0500

    47. Storing Data models in separate files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,13 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from './tasks/tasks.component';
-import { NgFor, NgIf } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf]
+    imports: [HeaderComponent, UserComponent, TasksComponent]
 })
 
 export class AppComponent {
