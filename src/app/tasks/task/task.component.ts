ca6b02cc4ed5aee6ecbfea45c63aacc4485df6cb
Rahmiddin Asiydinov <81851084+rahmiddinasiydinov@users.noreply.github.com>
2024-06-23 21:14:04 +0500

    49. Deleting tasks

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import {type Task } from './task.model';
 
 
@@ -13,4 +13,10 @@ import {type Task } from './task.model';
 
 export class TaskComponent {
   @Input({required: true}) task!: Task
+
+  @Output() complete = new EventEmitter<string>()
+
+  onCompleteTask(){
+    this.complete.emit(this.task.id)
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,6 +11,7 @@ import {type User } from './user.model';
 export class UserComponent {
   @Input({required: true}) user!: User;
   @Output() select = new EventEmitter<string>()
+  @Input({required: true}) selected!: boolean
 
   get imagePath() {
     return 'assets/users/' + this.user.avatar
